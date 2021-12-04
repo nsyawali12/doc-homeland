2d7eb9f03c1dca255b5aae6c71f217c7a3575bcb
unknown <nsyawali12@gmail.com>
2021-12-05 03:31:19 +0700

    blm bisa 2-2 nya ditampilin

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 import json
 from flask import Flask, flash, request, redirect, url_for, jsonify, render_template, Response
 from werkzeug.utils import secure_filename, send_from_directory
-import gc_homeland_v9
-from gc_homeland_v9 import read_preprocessing, ocr_phase
+import gc_homeland_v11
+import gc_homeland_v12
+from gc_homeland_v11 import read_preprocessing, ocr_json_phase
+from gc_homeland_v12 import read_preprocessing, ocr_tuple_phase
 # from flask_ngrok import run_with_ngrok
 
 UPLOAD_FOLDER = './upload'
@@ -17,15 +19,25 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def ocr_for_pdf(filename_for_pdf):
+def ocr_for_pdf_json(filename_for_pdf):
   get_pre_img = read_preprocessing(filename_for_pdf)
-  get_OCR_json = ocr_phase(get_pre_img)
+  get_OCR_json = ocr_json_phase(get_pre_img)
 
   # dict_sampe = {
   #   "file_output": filename_for_pdf,
   # }
   
   return get_OCR_json
+
+def ocr_for_pdf_tuple(filename_for_pdf):
+  get_pre_img_tuple = read_preprocessing(filename_for_pdf)
+  get_OCR_tuple = ocr_tuple_phase(get_pre_img_tuple)
+
+  # dict_sampe = {
+  #   "file_output": filename_for_pdf,
+  # }
+  
+  return get_OCR_tuple
 
 # Bagian GET
 @app.route('/', methods=['GET'])
@@ -61,14 +73,29 @@ def upload_file():
           filename = secure_filename(file.filename)
           file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
           file.save(file_path)
+
+          res_ocr = []
         
-          do_ocr_things = ocr_for_pdf(file_path)
+          do_ocr_json = ocr_for_pdf_json(file_path)
+          do_ocr_tuple = ocr_for_pdf_tuple(file_path)
 
           print("Request ke 3 aman")
-          respon = str(do_ocr_things)
+          
+          respon_json = str(do_ocr_json)
+          res_json = Response(respon_json, mimetype='application/json')
+          res_json.headers["Content-Disposition"] = "attachment;filename=ocr_json.json"
+
+          res_ocr.append(res_json)
+
+          print("request ocr json aman ")
+
+          respon = str(do_ocr_tuple)
           res = Response(respon, mimetype='application/json')
-          res.headers["Content-Disposition"] = "attachment;filename=hehe.json"
-          return res
+          res.headers["Content-Disposition"] = "attachment;filename=ocr_tuple.json"
+
+          print("request ocr tuple aman ")
+
+          return res_json
 
       return "Request ocr lancar"
 
